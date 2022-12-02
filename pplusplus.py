c5e3c269c610741ee7ffec286af17c4edfe1570d
Florian Fink <flfiit02@hs-esslingen.de>
2022-12-01 20:59:54 -0600

    Output File Code added

--- pplusplus.py
+++ pplusplus.py
@@ -7,8 +7,18 @@ class PPlusPlus:
         if arglen > 1 or arglen == 0:
             print("Usage python pplusplus.py [File]")
         else:
-            self.compile(sys.arg[1])
+            self.compile(sys.argv[1])
 
     def compile(self, path):
         file = open(path, "r", encoding="utf8")
         bytes = file.read()
+        file.close()
+
+        oPath = path[:-4] + "cpp"
+        output = open(oPath, "w")
+        output.write("test")
+        output.close()
+
+
+if __name__ == "__main__":
+    PPlusPlus()
