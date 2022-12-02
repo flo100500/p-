9fce23126d69ae5c2a5a535d2d73bcd16fb3b8ac
Florian Fink <flfiit02@hs-esslingen.de>
2022-12-01 20:08:37 -0600

    Base Structure

--- pplusplus.py
+++ pplusplus.py
@@ -1,1 +1,14 @@
-print ("Bonjour")
+import sys
+
+class PPlusPlus:
+    def __init__(self):
+        arglen = len(sys.argv) - 1
+
+        if arglen > 1 or arglen == 0:
+            print("Usage python pplusplus.py [File]")
+        else:
+            self.compile(sys.arg[1])
+
+    def compile(self, path):
+        file = open(path, "r", encoding="utf8")
+        bytes = file.read()
