fe20acdb880f1fd955f3dc9fdfc030a051833c49
AshleyDanielUlumben <ash.racers@gmail.com>
2022-12-01 22:58:40 -0600

    End of File

--- pplusplus.py
+++ pplusplus.py
@@ -3,6 +3,9 @@ import sys
 class PPlusPlus:
     def __init__(self):
         arglen = len(sys.argv) - 1
+        self.start = 0
+        self.current = 0
+        self.line = 1
 
         if arglen > 1 or arglen == 0:
             print("Usage python pplusplus.py [File]")
@@ -12,3 +15,5 @@ class PPlusPlus:
     def compile(self, path):
         file = open(path, "r", encoding="utf8")
         bytes = file.read()
+        
+            
