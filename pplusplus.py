113576bcd5da6b22a41ce1b16d73863d036f75d8
Florian Fink <flfiit02@hs-esslingen.de>
2022-12-01 22:44:59 -0600

    added {} => \t

--- pplusplus.py
+++ pplusplus.py
@@ -2,6 +2,8 @@ import sys
 
 class PPlusPlus:
     def __init__(self):
+        self.t = 0
+
         arglen = len(sys.argv) - 1
 
         if arglen > 1 or arglen == 0:
@@ -15,10 +17,38 @@ class PPlusPlus:
         file.close()
 
         oPath = path[:-4] + "cpp"
-        output = open(oPath, "w")
-        output.write("test")
-        output.close()
+        self.output = open(oPath, "w")
 
+        startL = True
+        tNum = 0
+
+        for byte in bytes:
+            match(byte):
+                case '\n':
+                    startL = True
+                    tNum = 0
+                case ' ':
+                    if startL: tNum =+ 1
+                case _:
+                    if startL: self.evaluateTabs(tNum)
+                    startL = False
+
+            self.output.write(byte)
+            
+
+        self.output.close()
+
+    def evaluateTabs(self, n):
+        diff = self.t - n
+        if(self.t > n): 
+            for i in range(diff):
+                self.output.write("}\n")
+        else:
+            for i in range(-diff):
+                self.output.write("{\n")
+                self.output.write("\t"*n)
+
+        self.t = n
 
 if __name__ == "__main__":
     PPlusPlus()
